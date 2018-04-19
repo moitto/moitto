aa7044a1438304dd0b30e888dfb87e54062e43e6
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-04-19 15:51:02 +0900

    Add userpic images.

--- catalogs/MainApp/feeds/catalog_feeds.js
+++ catalogs/MainApp/feeds/catalog_feeds.js
@@ -5,9 +5,9 @@ function feed_feeds(keyword, location, length, sortkey, sortorder, handler) {
        var data = [];
 
         discussions.forEach(function(discussion) {
-            var userpic_url       = __fetch_userpic_url_in_discussion(discussion);
-            var userpic_large_url = __fetch_userpic_large_url_in_discussion(discussion);;
-            var payout_value      = __fetch_payout_value_in_discussion(discussion).toFixed(2);
+            var userpic_url       = __get_userpic_url_in_discussion(discussion);
+            var userpic_large_url = __get_userpic_large_url_in_discussion(discussion);;
+            var payout_value      = __get_payout_value_in_discussion(discussion).toFixed(2);
 
             data.push({
                 "id":"S_FEEDS_" + discussion["author"] + "_" + discussion["permlink"],
@@ -27,15 +27,15 @@ function feed_feeds(keyword, location, length, sortkey, sortorder, handler) {
     });
 }
 
-function __fetch_userpic_url_in_discussion(discussion) {
+function __get_userpic_url_in_discussion(discussion) {
     return "https://steemitimages.com/u/" + discussion["author"] + "/avatar/small";
 }
 
-function __fetch_userpic_large_url_in_discussion(discussion) {
+function __get_userpic_large_url_in_discussion(discussion) {
     return "https://steemitimages.com/u/" + discussion["author"] + "/avatar";
 }
 
-function __fetch_payout_value_in_discussion(discussion) {
+function __get_payout_value_in_discussion(discussion) {
     var total_payout_value = parseFloat(discussion["total_payout_value"].replace("SBD", "").trim());
     
     if (total_payout_value > 0) {

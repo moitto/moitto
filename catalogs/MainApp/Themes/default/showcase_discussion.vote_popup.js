f23f5a3581e157a2d890df678491f745f50a52a1
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-05-07 09:45:16 +0900

    Add 'voted' value.

--- catalogs/MainApp/Themes/default/showcase_discussion.vote_popup.js
+++ catalogs/MainApp/Themes/default/showcase_discussion.vote_popup.js
@@ -1,8 +1,12 @@
 var account = require("account");
 
 function vote(form) {
-	account.vote($data["author"], $data["permlink"], 10000, function(response) {
-		controller.action("toast", { message:"보팅했습니다." });
-        controller.action("popup-close");
+	view.data("display-unit", {
+		"voted":"yes"
 	});
+
+	//account.vote($data["author"], $data["permlink"], 10000, function(response) {
+	//	controller.action("toast", { message:"보팅했습니다." });
+    //  controller.action("popup-close");
+	//});
 }
---
+++ catalogs/MainApp/Themes/default/showcase_discussion_page.js
@@ -0,0 +1,23 @@
+function update_vote() {
+	var voted = __is_voted();
+
+	__update_vote_button(voted);
+}
+
+function __update_vote_button(voted) {
+	var button = view.object("btn.vote");
+
+	button.property({
+		"selected":voted ? "yes" : "no"
+	});
+}
+
+function __is_voted() {
+	var data = view.data("display-unit");
+
+	if (data.hasOwnProperty("voted") && data["voted"] === "yes") {
+		return true;
+	}	
+
+	return false;
+}

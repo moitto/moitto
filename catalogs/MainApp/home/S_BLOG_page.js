4201cc0a95b5422fe4a943e79c3ddc1dfe581fd4
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-06-06 12:43:45 +0900

    Add view for user's blog.

--- catalogs/MainApp/home/S_BLOG_page.js
+++ catalogs/MainApp/home/S_BLOG_page.js
@@ -1,14 +1,51 @@
 var global = require("global");
 
-function on_loaded() {
-    var user = controller.catalog().value("showcase", "auxiliary", "S_BLOG");
+var __last_discussion = null;
 
-    global.get_user(user["username"]).then(function(user) {
-        var data = {
-            "fetched":"yes"
-        };
+function feed_blog(keyword, location, length, sortkey, sortorder, handler) {
+    var start_author   = (location > 0) ? __last_discussion["author"]   : null;
+    var start_permlink = (location > 0) ? __last_discussion["permlink"] : null;
 
-        view.data("display-unit", data);
-        view.action("reload");
+    global.steemjs.get_discussions_by_created($data["tag"], start_author, start_permlink, length).then(function(discussions) {
+        var data = [];
+
+        if (location > 0) {
+            discussions = discussions.splice(1);
+        }
+
+        discussions.forEach(function(discussion) {
+            var content   = global.contents.create(discussion);
+ 
+            data.push({
+                "id":"S_BLOG_" + content.data["author"] + "_" + content.data["permlink"],
+                "author":content.data["author"],
+                "permlink":content.data["permlink"],
+                "title":content.data["title"], 
+                "image-url":content.get_title_image_url("256x512"),
+                "userpic-url":content.get_userpic_url("small"),
+                "userpic-large-url":content.get_userpic_url(),
+                "author-reputation":content.get_author_reputation().toFixed(0).toString(),
+                "payout-value":"$" + content.get_payout_value().toFixed(2).toString(),
+                "votes-count":content.data["net_votes"].toString(),
+                "main-tag":content.data["category"],
+                "created-at":content.data["created"]
+            });
+        });
+
+        if (discussions.length > 0) {
+            __last_discussion = discussions[discussions.length - 1];
+        }
+
+        handler(data);
+    });        
+}
+
+function open_discussion(data) {
+    controller.catalog().submit("showcase", "auxiliary", "S_DISCUSSION", {
+        "author":data["author"],
+        "permlink":data["permlink"],
+        "userpic-url":data["userpic-url"]
     });
+    
+    controller.action("page", { "display-unit":"S_DISCUSSION" });
 }
--- catalogs/MainApp/home/showcase_user.blog_header.js
+++ catalogs/MainApp/home/showcase_user.blog_header.js
@@ -33,6 +33,33 @@ function unfollow() {
     });
 }
 
+function show_blog() {
+    controller.catalog().submit("showcase", "auxiliary", "S_BLOG", {
+        "username":$data["username"],
+        "fetched":"no"
+    });
+
+    controller.action("page", { "display-unit":"S_BLOG" });
+}
+
+function show_following() {
+    controller.catalog().submit("showcase", "auxiliary", "S_FOLLOWING", {
+        "username":$data["username"],
+        "fetched":"no"
+    });
+    
+    controller.action("page", { "display-unit":"S_FOLLOWING" });
+}
+
+function show_followers() {
+    controller.catalog().submit("showcase", "auxiliary", "S_FOLLOWERS", {
+        "username":$data["username"],
+        "fetched":"no"
+    });
+
+    controller.action("page", { "display-unit":"S_FOLLOWERS" });
+}
+
 function __show_follow_button(id) {
     var button = view.object("btn." + id);
 

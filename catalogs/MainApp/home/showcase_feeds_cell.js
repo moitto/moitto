303c9f851cb5a813babe0691e09e1134bf222544
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-06-06 11:37:21 +0900

    Connect an action to the buttons. #1738, #1715

--- catalogs/MainApp/home/showcase_feeds_cell.js
+++ catalogs/MainApp/home/showcase_feeds_cell.js
@@ -1,8 +1,25 @@
-function show_user() {
-    controller.catalog().submit("showcase", "auxiliary", "S_USER", {
-        "username":$data["author"],
-        "fetched":"no"
+function show_tag() {
+    controller.catalog().submit("showcase", "auxiliary", "S_TAG", {
+        "tag":$data["main-tag"]
     });
 
-    controller.action("page", { "display-unit":"S_USER" })
+    controller.action("page", { "display-unit":"S_TAG" })
+}
+
+function show_votes() {
+    controller.catalog().submit("showcase", "auxiliary", "S_VOTES", {
+        "author":$data["author"],
+        "permlink":$data["permlink"]
+    });
+
+    controller.action("page", { "display-unit":"S_VOTES" })
+}
+
+function show_replies() {
+    controller.catalog().submit("showcase", "auxiliary", "S_REPLIES", {
+        "author":$data["author"],
+        "permlink":$data["permlink"]
+    });
+
+    controller.action("page", { "display-unit":"S_REPLIES" })
 }

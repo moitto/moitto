a356b02fffd62a71fed387a6f05079925dfb1d9b
Seonae Gim <seonaegim@MacBook-Pro.local>
2018-06-26 17:16:27 +0900

    Fix logout

--- catalogs/MainApp/wallet/catalog_wallet_navibar.js
+++ catalogs/MainApp/wallet/catalog_wallet_navibar.js
@@ -6,3 +6,12 @@ function qrcode() {
     });
     controller.action("popup", { "display-unit":"S_QRCODE" });
 }
+
+function show_settings() {
+	controller.catalog().submit("showcase", "auxiliary", "S_ACCOUNT", {
+		"logged-in":account.is_logged_in() ? "yes" : "no"
+	});
+	
+	controller.action("subview", { "subview":"V_SETTINGS",
+								   "target":"popup" });
+}

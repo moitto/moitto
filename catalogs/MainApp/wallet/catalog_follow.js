7b12ca7a693ce12f42ec10ebc5255aca768102db
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-06-29 10:01:59 +0900

    Refine the follow view.

--- catalogs/MainApp/wallet/catalog_follow.js
+++ catalogs/MainApp/wallet/catalog_follow.js
@@ -2,8 +2,6 @@ var account = require("account");
 var users   = require("users");
 var steemjs = require("steemjs");
 
-var __last_label_for_follow_buttons = {};
-
 function on_loaded() {
     var value = controller.catalog().value("showcase", "auxiliary", "S_FOLLOW");
     var username = value["following"];
@@ -14,17 +12,32 @@ function on_loaded() {
     ]).then(function(response) {
         var user = users.create(username, response[0][0]);
         var followed = (response[1].length == 0 || response[1][0]["follower"] !== account.get_username()) ? false : true;
-        
-        controller.catalog().value("showcase", "auxiliary", "S_FOLLOW", Object.assign(value, {
+ 
+        __reload_follow_cell({
+            "username":username,
             "userpic-url":user.get_userpic_url(),
             "reputation":user.get_reputation().toFixed(1).toString(),
             "followed":followed ? "yes" : "no" 
         });
 
+        __show_follow_cell();
         __hide_loading_section();
     }, function(reason) {
-        // TBD       
+        // TBD
     });
+}
+
+function __reload_follow_cell(data) {
+    var cell = view.object("cell.follow");
+
+    cell.data("display-unit", data);
+    cell.action("reload");
+}
+
+function __show_follow_cell() {
+    var cell = view.object("cell.follow");
+
+    cell.action("show");
 }
 
 function __hide_loading_section() {

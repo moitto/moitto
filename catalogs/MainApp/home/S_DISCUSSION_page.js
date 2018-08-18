4991590ca10abc0ffd671b58e13d5b7beed93d68
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-08-18 15:51:44 +0900

    Fix the bug.

--- catalogs/MainApp/home/S_DISCUSSION_page.js
+++ catalogs/MainApp/home/S_DISCUSSION_page.js
@@ -8,8 +8,8 @@ var __disallowed_tags = safety.get_disallowed_tags();
 function on_loaded() {
     var discussion = controller.catalog().value("showcase", "auxiliary", "S_DISCUSSION");
     
-    discussion["author"] = "eternalight";
-    discussion["permlink"] = "7pn3rs-500";
+    //discussion["author"] = "eternalight";
+    //discussion["permlink"] = "7pn3rs-500";
 
     __get_content(discussion["author"], discussion["permlink"], function(content) {
         var me = storage.value("ACTIVE_USER") || "";

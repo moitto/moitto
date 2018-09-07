7c02ef654dc0e0488a6dac297d2aa116666d1b69
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-09-07 21:22:17 +0900

    Refine the replies view.

--- catalogs/MainApp/home/S_REPLIES_page.js
+++ catalogs/MainApp/home/S_REPLIES_page.js
@@ -4,41 +4,36 @@ var themes  = require("themes");
 
 function on_loaded() {
     var value = controller.catalog().value("showcase", "auxiliary", "S_REPLIES");
-    var path = "/" + value["tag"] + "/@" + value["author"] + "/" + value["permlink"];
     var me = storage.value("ACTIVE_USER") || "";
 
-    steemjs.get_state(path).then(function(response) {
+    __get_replies(value["tag"], value["author"], value["permlink"], function(response) {
         var theme = themes.create("default");
         var replies_text = "";
         var replies_data = [];
 
-        Object.keys(response["content"]).forEach(function(path) {
-            var content = response["content"][path];
+        response.forEach(function(content) {
+            var reply = replies.create(content);
+            var datum = {
+                "id":"S_REPLIES_" + reply.data["permlink"],
+                "author":reply.data["author"], 
+                "permlink":reply.data["permlink"], 
+                "parent-author":reply.data["parent_author"],
+                "parent-permlink":reply.data["parent_permlink"],
+                "userpic-url":reply.get_userpic_url("small"),
+                "body":theme.build_body(reply.data["body"], "markdown", []),
+                "body-text":reply.data["body"], 
+                "payout-value":"$" + reply.get_payout_value().toFixed(2).toString(),
+                "payout-done":reply.is_payout_done() ? "yes" : "no",
+                "replies-count":reply.data["children"].toString(),
+                "created-at":reply.data["created"],
+                "vote-weight":reply.get_vote_weight(me).toString(),
+                "editable":reply.is_editable(me) ? "yes" : "no",
+                "deletable":reply.is_deletable(me) ? "yes" : "no",
+                "hidable":(reply.is_hidable(me) && !reply.is_owner(me)) ? "yes" : "no"
+            }
 
-            if (content["parent_permlink"] === value["permlink"] && content["parent_permlink"] != content["permlink"]) {
-                var reply = replies.create(content);
-                var datum = {
-                    "id":"S_REPLIES_" + reply.data["permlink"],
-                    "author":reply.data["author"], 
-                    "permlink":reply.data["permlink"], 
-                    "parent-author":reply.data["parent_author"],
-                    "parent-permlink":reply.data["parent_permlink"],
-                    "userpic-url":reply.get_userpic_url("small"),
-                    "body":theme.build_body(reply.data["body"], "markdown", []),
-                    "body-text":reply.data["body"], 
-                    "payout-value":"$" + reply.get_payout_value().toFixed(2).toString(),
-                    "payout-done":reply.is_payout_done() ? "yes" : "no",
-                    "replies-count":reply.data["children"].toString(),
-                    "created-at":reply.data["created"],
-                    "vote-weight":reply.get_vote_weight(me).toString(),
-                    "editable":reply.is_editable(me) ? "yes" : "no",
-                    "deletable":reply.is_deletable(me) ? "yes" : "no",
-                    "hidable":(reply.is_hidable(me) && !reply.is_owner(me)) ? "yes" : "no"
-                }
-
-                if (!reply.is_banned() && !reply.is_down_voted(value["author"]) ) {
-                    replies_data.push(datum);
-                }
+            if (!reply.is_banned() && !reply.is_down_voted(value["author"]) ) {
+                replies_data.push(datum);
             }
         });
 
@@ -65,3 +60,27 @@ function on_loaded() {
         view.action("reload"); 
     });
 }
+
+function __get_replies(tag, author, permlink, handler) {
+    var path = "/" + tag + "/@" + author + "/" + permlink;
+    var replies = [];
+
+    steemjs.get_state(path).then(function(response) {
+        if (response) {
+            Object.keys(response["content"]).forEach(function(path) {
+                var content = response["content"][path];
+
+                if (content["parent_permlink"] === permlink && content["parent_permlink"] != content["permlink"]) {
+                    replies.push(content);
+                }
+            });
+
+            handler(replies);            
+        } else {
+            handler();
+        }
+    }, function(reason) {
+        handler();
+    });
+}
+

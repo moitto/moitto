e7c3932185188c3d56c6eec7d1c15ea369dbb0c8
Hanyeol Cho <hanyeol.cho@gmail.com>
2018-09-09 14:22:09 +0900

    Fix the bug.

--- catalogs/MainApp/home/S_COMMENT_popup.js
+++ catalogs/MainApp/home/S_COMMENT_popup.js
@@ -5,7 +5,7 @@ function on_loaded() {
     var value = controller.catalog().value("showcase", "auxiliary", "S_COMMENT");
     var identifier = "S_COMMENTS_" + value["parent-author"] + "_" + value["parent-permlink"];
     var temporary = controller.catalog().value("showcase", "comments", identifier);
-    var text = temporary ? temporary["text"] : value["text"].replace(__signature_pattern, "");
+    var text = temporary ? temporary["text"] : (value["text"] || "").replace(__signature_pattern, "");
 
     view.object("editor").property({ "text":text });
 }
@@ -15,7 +15,7 @@ function close() {
     var editor = view.object("editor");
     var text = editor.value();
 
-    if (text.length > 0 && text !== value["text"].replace(__signature_pattern, "")) {
+    if (text.length > 0 && text !== (value["text"] || "").replace(__signature_pattern, "")) {
         controller.action("prompt", {
             "title":"알림",
             "message":"이 댓글을 임시 저장하시겠어요?",
